e40a4ba103a18a58847e56545de474c79da10470
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-03-25 13:16:44 +0600

    add: first pattern

--- Pattern.java
+++ Pattern.java
@@ -1,5 +1,26 @@
 class Pattern {
     public static void main(String args[]) {
-        
+        //?   * * * * *
+        //?   * * * * *
+        //?   * * * * *
+        //?   * * * * *
+
+        for (int i = 0; i < 4; i++) {
+            String str = "";
+
+            for (int j = 1; j <= 5; j++) {
+                str += "* ";
+            }
+
+            System.out.println(str);
+        }
+
+        // solution 2
+        // for (int i = 0; i < 4; i++) {
+        //     for (int j = 1; j <= 5; j++) {
+        //         System.out.print(" * ");
+        //     }
+        //         System.out.println();
+        // }
     }
 }
