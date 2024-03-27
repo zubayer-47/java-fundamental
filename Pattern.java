b0c060ab2b765dc8bb8ae4f3ca8b177de7ccb49f
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-03-27 17:11:45 +0600

    add: 0-1 Triangle Pattern

--- Pattern.java
+++ Pattern.java
@@ -182,26 +182,55 @@ class Pattern {
         // }
 
 
-        //* Floyd's Triangle
+        // //* Floyd's Triangle
 
-        //? 1
-        //? 2   3
-        //? 4   5   6
-        //? 7   8   9   10
-        //? 11  12  13  14  15
+        // //? 1
+        // //? 2   3
+        // //? 4   5   6
+        // //? 7   8   9   10
+        // //? 11  12  13  14  15
+
+        // int n = 5;
+        // int num = 1;
+
+        // for (int i = 1; i <= n; i++) {
+        //     String str = "";
+
+        //     for (int j = 1; j <= i; j++) {
+        //         str += num + " ";
+        //         num++;
+        //     }
+
+        //     System.out.println(str);
+        // }
+
+         //* 0 - 1 Triangle
+
+        //?     1
+        //?     0 1
+        //?     1 0 1
+        //?     0 1 0 1
+        //?     1 0 1 0 1
+
+        //? Let's assume. Even = 1; Odd = 0;
 
         int n = 5;
-        int num = 1;
 
         for (int i = 1; i <= n; i++) {
+
             String str = "";
 
             for (int j = 1; j <= i; j++) {
-                str += num + " ";
-                num++;
+                // System.out.print(i % j);
+                if ((i+j) % 2 == 0) {
+                    str += 1 + " ";
+                } else {
+                    str += 0 + " ";
+                }
             }
 
             System.out.println(str);
+
         }
     }
 }
